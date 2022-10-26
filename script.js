ed93f63e7ad8ae74766efb2fa5c6b21afc2079c2
Jibek1906 <73746820+Jibek1906@users.noreply.github.com>
2022-10-26 14:26:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.getElementById('button').onclick = () => {
 counter = counter + 1;
 document.getElementById('score').innerText = counter;
 };
+
+function myFunction() {
+    document.getElementById("no").innerHTML = "Эмне?";
+  }

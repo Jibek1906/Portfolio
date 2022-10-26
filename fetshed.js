f158f59818c58c1ebe40e7f4e07a3089eab82abd
Jibek1906 <73746820+Jibek1906@users.noreply.github.com>
2022-10-26 14:27:31 +0600

    Add files via upload

---
+++ fetshed.js
@@ -0,0 +1,16 @@
+
+fetch("https://jsonplaceholder.typicode.com/users")
+
+    .then(response => response.json())
+    .then(json => {
+
+        let li = `<tr><th>Name</th><th>Email</th></tr>`;
+        json.forEach(user => {
+            li += `<tr>
+                <td>${user.name} </td>
+                <td>${user.email}</td>        
+            </tr>`;
+        });
+  
+    document.getElementById("users").innerHTML = li;
+});
